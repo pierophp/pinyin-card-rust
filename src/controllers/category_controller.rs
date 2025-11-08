736e645edfc4ed6201a0c1fe1edf7757e5fa8bde
Piero Giusti <pierophp@gmail.com>
2025-11-08 13:08:25 -0300

    Categories

--- src/controllers/category_controller.rs
+++ src/controllers/category_controller.rs
@@ -1,17 +1,31 @@
 use crate::models::category::Category;
 use axum::{
-    extract::State,
+    Json,
+    extract::{Query, State},
     http::StatusCode,
     response::IntoResponse,
-    Json,
 };
+use serde::Deserialize;
 use sqlx::MySqlPool;
+
+#[derive(Deserialize)]
+pub struct CategoryQuery {
+    id: Option<i32>, // Optional parameter
+}
 
 /// Handler to get all categories from the database
 pub async fn get_categories(
     State(pool): State<MySqlPool>,
+    Query(params): Query<CategoryQuery>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let categories = sqlx::query_as::<_, Category>("SELECT * FROM category")
+    let where_clause = match params.id {
+        Some(id) => format!("parent_category_id = {}", id),
+        None => "parent_category_id IS NULL".to_string(),
+    };
+
+    let query = format!("SELECT * FROM category WHERE {}", where_clause);
+
+    let categories = sqlx::query_as::<_, Category>(&query)
         .fetch_all(&pool)
         .await
         .map_err(|e| {
@@ -24,4 +38,3 @@ pub async fn get_categories(
 
     Ok(Json(categories))
 }
-
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,12 +1,20 @@
 use crate::controllers::{category_controller, hello_controller};
-use axum::{Router, routing::get};
+use axum::{Router, http::HeaderValue, routing::get};
 use sqlx::MySqlPool;
+use tower_http::cors::{Any, CorsLayer};
 
 /// Configure all application routes
 pub fn configure_routes(pool: MySqlPool) -> Router {
+    // Configure CORS to allow requests from localhost:5173
+    let cors = CorsLayer::new()
+        .allow_origin(HeaderValue::from_static("http://localhost:5173"))
+        .allow_methods(Any)
+        .allow_headers(Any);
+
     Router::new()
         .route("/", get(hello_controller::hello_world))
         .route("/health", get(hello_controller::health_check))
         .route("/categories", get(category_controller::get_categories))
+        .layer(cors)
         .with_state(pool)
 }
